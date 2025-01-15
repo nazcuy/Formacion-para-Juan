b47df9de2afcd7c39bbf2f2981af89efa1936e88
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-01-14 23:51:13 -0300

    Subo extensión nueva

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx-pdj-theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,6 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
